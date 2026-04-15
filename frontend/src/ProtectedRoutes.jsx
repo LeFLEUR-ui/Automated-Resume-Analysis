75815899c9a9436efbcb3e8febacb0314405145a
Marvin Fabricante <marvinfabricante630@gmail.com>
2026-04-15 17:27:21 +0800

    Adding protected routes

---
+++ frontend/src/ProtectedRoutes.jsx
@@ -0,0 +1,18 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+const ProtectedRoute = ({ allowedRole }) => {
+  const token = localStorage.getItem('token');
+  const userRole = localStorage.getItem('role');
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (allowedRole && userRole !== allowedRole) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
