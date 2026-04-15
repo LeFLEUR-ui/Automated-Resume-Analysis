7f359638af5ac111cf8b7d313e95b248815e5924
Marvin Fabricante <marvinfabricante630@gmail.com>
2026-04-15 17:54:28 +0800

    Adding admin model

---
+++ backend/app/models/admin.py
@@ -0,0 +1,15 @@
+from sqlalchemy import *
+
+from app.models.user import User
+
+
+class Admin(User):
+    __tablename__ = "admins"
+
+    id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+
+    managed_region = Column(String, nullable=True)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "admin",
+    }
