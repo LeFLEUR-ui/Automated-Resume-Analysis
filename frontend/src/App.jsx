6452182d726fb7fa1c45f7a2e11294d38c8f4f8e
Marvin Fabricante <marvinfabricante630@gmail.com>
2026-04-16 10:50:56 +0800

    Making App.jsx codebase clean and routes clean

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 
 import ProtectedRoute from './ProtectedRoutes';
-import Register from './pages/main_page/Register';
-import Login from './pages/main_page/Login';
-import LandingPage from './pages/main_page/LandingPage';
-import AboutPage from './pages/main_page/AboutPage';
-import CareersPage from './pages/main_page/CareersPage';
+import Register from './pages/publicPages/Register';
+import Login from './pages/publicPages/Login';
+import LandingPage from './pages/publicPages/LandingPage';
+import AboutPage from './pages/publicPages/AboutPage';
+import CareersPage from './pages/publicPages/CareersPage';
 
 import HRDashboard from './pages/hr/HRDashboard';
 import ScreeningPortal from './pages/hr/ScreeningPortal';
@@ -13,24 +13,22 @@ import JobManagement from './pages/hr/JobManagement';
 
 import ApplyPage from './pages/candidate/ApplyPage';
 import CandidateDashboard from './pages/candidate/CandidateDashboard';
+import ApplicationTracking from './pages/candidate/ApplicationTracking';
+import FindJob from './pages/candidate/FindJob';
 
 import AdminDashboard from './pages/admin/AdminDashboard';
 import AuditLogPage from './pages/admin/AuditLog';
 import UsersPage from './pages/admin/Users';
-import ApplicationTracking from './pages/candidate/ApplicationTracking';
-import FindJob from './pages/candidate/FindJob';
 
-function App() {
+const App = () => {
   return (
     <Routes>
-      {/* Mariwasa Pages */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/" element={<LandingPage />} />
       <Route path="/careerspage" element={<CareersPage />} />
       <Route path="/aboutpage" element={<AboutPage />} />
 
-      {/* For Admin's */}
       <Route element={<ProtectedRoute allowedRole="ADMIN" />}>
         <Route path="/admin">
           <Route path="dashboard" element={<AdminDashboard />} />
@@ -39,7 +37,6 @@ function App() {
         </Route>
       </Route>
 
-      {/* For HR's */}
       <Route element={<ProtectedRoute allowedRole="HR" />}> 
         <Route path="/hr">
           <Route path="dashboard" element={<HRDashboard />} />
@@ -48,7 +45,6 @@ function App() {
         </Route>
       </Route>
 
-      {/* For Candidates's */}
       <Route element={<ProtectedRoute allowedRole="CANDIDATE" />}> 
         <Route path="/candidate">
           <Route path="dashboard" element={<CandidateDashboard />} />
