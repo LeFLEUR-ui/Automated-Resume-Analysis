7ee580229619a76b8d70ee701326c7a8deb6136c
Marvin Fabricante <marvinfabricante630@gmail.com>
2026-04-22 06:29:59 +0800

    sorting all jobs descendingly

--- backend/app/services/job_description_service.py
+++ backend/app/services/job_description_service.py
@@ -1,6 +1,6 @@
-from sqlalchemy import desc
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy import desc
 from app.models.job_description import JobDescription
 from app.schemas.job_description_schema import JobCreate
 
@@ -12,17 +12,19 @@ async def create_job(db: AsyncSession, job: JobCreate):
     return db_job
 
 async def get_job(db: AsyncSession, job_id: str):
-    # ung jobs naka pabaligtad ung order niya, mauuna ung nasa pinaka huli na record which is ung newly created.
-    query = (
-        select(JobDescription)
-        .filter(JobDescription.job_id == job_id)
-        .order_by(desc(JobDescription.job_id))
-    )
-    
-    result = await db.execute(query)
+    result = await db.execute(select(JobDescription).filter(JobDescription.job_id == job_id))
     return result.scalars().first()
 
+
+# retrieving all jobs in descending order, bali mauuna ung newly created which is nasa pinaka dulo ng record.
 async def get_all_active_jobs(db: AsyncSession, skip: int = 0, limit: int = 100):
-    query = select(JobDescription).filter(JobDescription.is_active == True).offset(skip).limit(limit)
+    query = (
+        select(JobDescription)
+        .filter(JobDescription.is_active == True)
+        .order_by(desc(JobDescription.id))
+        .offset(skip)
+        .limit(limit)
+    )
+    
     result = await db.execute(query)
     return result.scalars().all()
